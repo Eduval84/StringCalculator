ecf93ba379a4b1abcd744282145b4edc67fd3c7c
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-21 23:36:50 +0000

    Red test String1ComaString2ShoudBe3

--- TestStringCalculator/StringCalculatorSpecs.cs
+++ TestStringCalculator/StringCalculatorSpecs.cs
@@ -14,5 +14,15 @@ namespace TestStringCalculator
 
             result.Should().Be(0);
         }
+
+        [Fact]
+        public void String1ComaString2ShoudBe3()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+
+            var result = stringCalculator.Add("1,2");
+
+            result.Should().Be(3);
+        }
     }
 }
