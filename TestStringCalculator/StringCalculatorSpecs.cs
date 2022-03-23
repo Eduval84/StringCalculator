79a35c0cf255901ec9f2e3a4e0c488ced609b591
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-23 22:33:44 +0000

    Red test numbers_greater_than_1000_must_be_ignored

--- TestStringCalculator/StringCalculatorSpecs.cs
+++ TestStringCalculator/StringCalculatorSpecs.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Xml.XPath;
 using FluentAssertions;
 using Xunit;
@@ -26,32 +27,32 @@ namespace TestStringCalculator
         [InlineData("1,2",3)]
         [InlineData("3,5", 8)]
         [InlineData("1,2,3", 6)]
-        public void given_positive_strings_numbers_separate_by_comas_get_a_int_result_with_the_sum_of_all_of_them(string givenNumbers, int expected)
+        public void given_positive_strings_numbers_separate_by_comas_get_a_int_result_with_the_sum_of_all_of_them(string givenNumbers, int expectedResutl)
         {
             _result = _stringCalculator.Add(givenNumbers);
 
-            _result.Should().Be(expected);
+            _result.Should().Be(expectedResutl);
         }
 
         [Theory]
         [InlineData("1\n2,4",7)]
         [InlineData("2\n2,3\n2", 9)]
-        public void given_positive_strings_numbers_separate_by_comas_or_scape_secuence_get_a_int_result_with_the_sum_of_all_of_them(string givenNumbers, int expected)
+        public void given_positive_strings_numbers_separate_by_comas_or_scape_secuence_get_a_int_result_with_the_sum_of_all_of_them(string givenNumbers, int expectedResult)
         {
             _result = _stringCalculator.Add(givenNumbers);
 
-            _result.Should().Be(expected);
+            _result.Should().Be(expectedResult);
         }
 
         [Theory]
         [InlineData("//;\n1;2",3)]
         [InlineData("//@\n3@3",6)]
-        public void allow_a_custom_delimiter_when_given_input_starts_with_double_bar(string givenInput, int expected)
+        public void allow_a_custom_delimiter_when_given_input_starts_with_double_bar(string givenInput, int expectedResult)
         {
           
             _result = _stringCalculator.Add(givenInput);
 
-            _result.Should().Be(expected);
+            _result.Should().Be(expectedResult);
         }
 
         [Theory]
@@ -62,6 +63,17 @@ namespace TestStringCalculator
             Action whenAct = () => _stringCalculator.Add(givenInput);
 
             whenAct.Should().Throw<NegativeNotAllowed>().Where(x => x.Message == expectedExceptionMessage);
+        }
+
+        [Theory]
+        [InlineData("1,1000,3",1004)]
+        [InlineData("1,1001,3",4)]
+
+        public void numbers_greater_than_1000_must_be_ignored(string givenInput, int expectedResult)
+        {
+            _result = _stringCalculator.Add(givenInput);
+
+            _result.Should().Be(expectedResult);
         }
     }
 }
